6e9b2cd2699662b0c719121df5ee95c10c2f5d40
Nidhi Singh <nidhi777786@gmail.com>
2025-03-22 17:31:47 +0530

    implement user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,22 @@
-import { Router } from 'express'
+import { Router } from 'express';
 const router = Router()
-import getUsers from "../controllers/userController"
+import { verifyIsLoggedIn, verifyIsAdmin } from "../middleware/verifyAuthToken";
+import { getUsers, registerUser, loginUser, updateUserProfile, getUserProfile, writeReview, getUser, updateUser, deleteUser } from "../controllers/userController";
 
+router.post("/register", registerUser)
+router.post("/login", loginUser)
+
+// user logged in routes:
+router.use(verifyIsLoggedIn);
+router.put("/profile", updateUserProfile);
+router.get('/profile/:id', getUserProfile)
+router.post('/review/:productId', writeReview)
+
+// admin routes:
+router.use(verifyIsAdmin);
 router.get("/", getUsers)
+router.get("/:id", getUser);
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,32 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express, { json } from "express";
+const app = express();
+const port = 3000;
 
-const apiRoutes = require("./routes/apiRoutes")
+app.use(json())
 
-app.get('/', async (req, res, next) => {
-    const Product = require("./models/ProductModel")
-    try {
-        const product = new Product
-        product.name = "New product name"
-        const productSaved = await product.save()
-        console.log(productSaved === product)
-        const products = await Product.find()
-        console.log(products.length)
-        res.send("Product created " + product._id)
-    } catch(er) {
-        next(er)
-    }
-    // res.json({message: "API running..."})
-})
+import apiRoutes from "./routes/apiRoutes";
+
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running..." });
+});
 
 // mongodb connection
-const connectDB = require("./config/db")
+import connectDB from "./config/db";
 connectDB();
 
-app.use('/api', apiRoutes)
+app.use("/api", apiRoutes);
 
 app.use((error, req, res, next) => {
-    console.error(error);
-    next(error)
-})
+  console.error(error);
+  next(error);
+});
 app.use((error, req, res, next) => {
-    res.status(500).json({
-        message: error.message,
-        stack: error.stack
-    })
-})
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
