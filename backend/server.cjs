598166dc1d1d82e25bcd3e990cb65054860fbb33
Nidhi Singh <nidhi777786@gmail.com>
2025-04-04 13:24:42 +0530

    fixed deployment error

--- backend/server.cjs
+++ backend/server.cjs
@@ -100,8 +100,8 @@ app.use("/api", apiRoutes);
 
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")));
+    app.use(express.static(path.join(__dirname, "../frontend2/dist")));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend2", "dist", "index.html")));
 } else {
    app.get("/", (req,res) => {
       res.json({ message: "API running..." }); 
